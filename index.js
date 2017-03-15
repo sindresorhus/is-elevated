c5cef4912c5eebd7aa5de92e3bd5078e1be75655
Sindre Sorhus <sindresorhus@gmail.com>
2017-03-16 02:49:39 +0700

    Simplify

--- index.js
+++ index.js
@@ -2,4 +2,4 @@
 const isRoot = require('is-root');
 const isAdmin = require('is-admin');
 
-module.exports = () => process.platform === 'win32' ? isAdmin() : Promise.resolve().then(() => isRoot());
+module.exports = () => process.platform === 'win32' ? isAdmin() : Promise.resolve().then(isRoot);
